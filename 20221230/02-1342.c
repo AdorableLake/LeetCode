41d93d2c71935dd1b831e4fec018a5a91a196ff8
志远 <50417000+AdorableLake@users.noreply.github.com>
2022-12-30 13:39:06 +0800

    Update 02-1342.c

--- 20221230/02-1342.c
+++ 20221230/02-1342.c
@@ -1,5 +1,5 @@
 int numberOfSteps(int num){
-    int i=1;
+    int i=0;
     while(num!=0){
         if(num%2==0){
             printf("步骤 %d) %d 是偶数，除以 2 得到 %d 。", i, num, num/2);
@@ -11,4 +11,5 @@ int numberOfSteps(int num){
             i++;
         } // if
     } // while
+    return i; // XD 原来LeetCode强制添加return
 } // 这里怎么就错了呢
